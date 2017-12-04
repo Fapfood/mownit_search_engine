35c9321fb0422bdfbc4cbba70cbde60dac216bfb
matr <maciej.trzebinski@grapeup.com>
2017-12-04 18:50:53 +0100

    add flask

--- engine.py
+++ engine.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from werkzeug.utils import redirect
+from wtforms import StringField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config.from_object('config')
+
+
+class SearchForm(FlaskForm):
+    words = StringField('words', validators=[DataRequired()])
+
+
+@app.route('/search', methods=['GET', 'POST'])
+def login():
+    form = SearchForm()
+    if form.validate_on_submit():
+        flash('{}'.format(form.words.data))
+        return redirect('/results')
+    return render_template('search.html', title='Search', form=form)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/results')
+def index():
+    return render_template("results.html", title='Home')
 
 
 if __name__ == '__main__':
