f414d20937193d3f6d8286a4b2ce1188116e7f65
matr <maciej.trzebinski@grapeup.com>
2017-12-22 17:50:28 +0100

    last fix

--- svds.py
+++ svds.py
@@ -22,9 +22,10 @@ from scipy.sparse.linalg import svds
 # V = np.load('resources/V_matrix.npy')
 # t_start = time()
 # m = np.zeros(shape)
-# for i, row in enumerate(m):
+# for i in range(length):
+#     print(i)
 #     for j in range(shape[0]):
-#         row += V[i].dot(s[i] * U[j, i])
+#         m[j] += V[i].dot(s[i] * U[j, i])
 # t_end = time()
 # print(t_end - t_start)
 # np.save('resources/cln_matrix', m)
@@ -51,13 +52,14 @@ s = np.load('resources/s_nonscale_but_normalise_matrix.npy')
 V = np.load('resources/V_nonscale_but_normalise_matrix.npy')
 t_start = time()
 m = np.zeros(shape)
-for i, row in enumerate(m):
+for i in range(length):
+    print(i)
     for j in range(shape[0]):
-        row += V[i].dot(s[i] * U[j, i])
+        m[j] += V[i].dot(s[i] * U[j, i])
 t_end = time()
 print(t_end - t_start)
 np.save('resources/cln_nonscale_but_normalise_matrix', m)
 
-# matrix = np.load('resources/cln_nonscale_but_normalise_matrixnpy').round(6)
+# matrix = np.load('resources/cln_nonscale_but_normalise_matrix.npy').round(6)
 # csc_matrix = csc_matrix(matrix)
 # save_npz('resources/cln_sparse_nonscale_but_normalise_matrix', csc_matrix)
